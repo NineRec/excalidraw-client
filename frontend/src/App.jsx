05fb945ff777a804c9aef1f166d4b7d169dbdec8
NineRec <gongshang1990@163.com>
2023-11-16 10:56:15 +0800

    fix some resource issue

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Excalidraw } from "@excalidraw/excalidraw";
+
+function App() {
+  return (
+    <>
+      <div style={{ height: "100vh", }}>
+        <Excalidraw />
+      </div>
+    </>
+  );
+}
+
+export default App
